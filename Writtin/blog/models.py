9c6b708260402a15517b9c3c17283ac653c90991
ahmedhalimx <ahmedhalimcs101@gmail.com>
2025-01-06 01:49:18 +0200

    Created 'blog' app

--- Writtin/blog/models.py
+++ Writtin/blog/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+    content = models.TextField(max_length=12000)
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
